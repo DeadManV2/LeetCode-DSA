53bb6a2a4bd9b5d9f6c79516ac06ff84e39b6598
DeadManV2 <madan.pratap1211@gmail.com>
2026-07-24 23:37:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (30.52%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,12 +1,22 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int ans = 0;
-        int max = -1;
-        for(int i = 0; i < arr.length; i++){
-            max = Math.max(max, arr[i]);
-            if(max == i) ans++;
+
+    Stack<Integer> stack = new Stack<>();
+
+    for(int x : arr){
+        if(stack.isEmpty() || x >= stack.peek()){
+            stack.push(x);
         }
-        return ans;
+        else{
+            int max = stack.pop();
+            while(!stack.isEmpty() && stack.peek() > x){
+                stack.pop();
+            }
+            stack.push(max);
+        }
+
+    }
+    return stack.size();
         
     }
 }
