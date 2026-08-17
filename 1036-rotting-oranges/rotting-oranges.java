7378344769982797f89e9860c15b9f5634e2ac03
DeadManV2 <madan.pratap1211@gmail.com>
2026-08-17 14:03:24 +0530

    Time: 2 ms (89.58%) | Memory: 44.4 MB (13.79%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -31,18 +31,18 @@ class Solution {
                 if(nrow < 0 || nrow >= m || ncol < 0 || ncol >= n) {
                     continue;
                 }
-                if(grid[nrow][ncol] == 1) {
+                if(grid[nrow][ncol] == 1 && !vis[nrow][ncol]) {
                     queue.offer(new Pair(nrow, ncol));
                     time[nrow][ncol] = time[row][col] + 1;
-                    //vis[nrow][ncol] = true;
-                    grid[nrow][ncol] = 2;
+                    vis[nrow][ncol] = true;
+                  //  grid[nrow][ncol] = 2;
                 }
             }
         }
         int ans = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) return -1;
+                if(grid[i][j] == 1 && !vis[i][j]) return -1;
 
                 ans = Math.max(ans, time[i][j]);
             }
