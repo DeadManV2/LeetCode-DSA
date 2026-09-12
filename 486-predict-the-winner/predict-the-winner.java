57b793e9876d84f2c9421a2a9309ee208e3efeac
DeadManV2 <madan.pratap1211@gmail.com>
2026-09-12 20:48:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (18.48%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,5 +1,7 @@
 class Solution {
 
+    private int[][] dp;
+
     private int solve(int[] nums, int i, int j) {
 
         if (i > j) {
@@ -9,6 +11,9 @@ class Solution {
         if (i == j) {
             return nums[i];
         }
+        if(dp[i][j] != Integer.MIN_VALUE) {
+            return dp[i][j];
+        }
 
         // Take left
         int takeLeft = nums[i] - solve(nums, i + 1, j);
@@ -16,12 +21,17 @@ class Solution {
         // Take right
         int takeRight = nums[j] - solve(nums, i, j - 1);
 
-        return Math.max(takeLeft, takeRight);
+        return dp[i][j] = Math.max(takeLeft, takeRight);
     }
 
     public boolean predictTheWinner(int[] nums) {
-
+       int n = nums.length;
+       dp = new int[n][n];
+        for(int i = 0; i < nums.length; i++){
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
         int result = solve(nums, 0, nums.length - 1);
+
 
         return result >= 0;
     }
