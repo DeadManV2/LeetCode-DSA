aca3ccb11e48718e384cb5fa7019ba40ae415c90
DeadManV2 <madan.pratap1211@gmail.com>
2026-09-23 21:14:04 +0530

    Time: 151 ms (5.23%) | Memory: 46.9 MB (52.33%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    void twoSum(int n, long target, int[] n3, int[] n4){
+
+    }
+    public int fourSumCount(int[] n1, int[] n2, int[] n3, int[] n4) {
+
+        int n = n1.length;
+        int res = 0;
+        Map<Long, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+              
+              long target = -(n1[i] + n2[j]);
+              map.put(target, map.getOrDefault(target, 0) + 1);
+            }
+        }
+        for(int i = 0; i < n ; i++) {
+            for(int j = 0; j < n; j++){
+                long sum = n3[i] + n4[j];
+                if(map.containsKey(sum)){
+                    res += map.get(sum);
+                }
+            }
+        }
+        return res;
+
+
+        
+    }
+}
